4b9808fdbeb7427071dfb4643d9bc5dcec49fd8c
SujinRoh <suuz.hha@gmail.com>
2021-10-31 18:56:46 +0900

    basic setting

---
+++ PizzaBoy_0.0.1/pizzaBoy/pbdw/src/main/java/com/sujin/pbdw/PbdwApplication.java
@@ -0,0 +1,13 @@
+package com.sujin.pbdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbdwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbdwApplication.class, args);
+	}
+
+}
---
+++ PizzaBoy_0.0.1/pizzaBoy/pbdw/src/test/java/com/sujin/pbdw/PbdwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujin.pbdw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbdwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
